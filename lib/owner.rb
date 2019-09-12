5ffdbcc885e9e32b51eccf40dc9f4f0ae8a9e7f3
David Strauch <dmaster18@gmail.com>
2019-09-12 02:38:35 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -33,7 +33,7 @@ class Owner
   end
   
   def buy_cat(name)
-    purchased_cat = Cat.all.find{|cat|cat if cat.name == name && cat.owner != self}
+    purchased_cat = Cat.all.find{|cat|cat if cat.name == name}
     purchased_cat.owner = self
   end
   
